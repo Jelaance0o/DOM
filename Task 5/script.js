52816f3272c3f4dfeda74b757a3a96e1681c7629
Jelaance0o <162440781+Jelaance0o@users.noreply.github.com>
2025-11-23 17:37:29 +0530

    Downloading button

--- Task 5/script.js
+++ Task 5/script.js
@@ -1,23 +1,23 @@
+var grow = 0
 var btn = document.querySelector('button')
 var h1 = document.querySelector('#bottom h1')
 var inside = document.querySelector('#inside')
-var count
 
 btn.addEventListener('click',function(){
-    btn.style.pointerEvents ='none'
-    count = 0
+    btn.style.pointerEvents = 'none'
+
+    var num = 50 + Math.floor(Math.random()*100)
     
     var int = setInterval(() => {
-        count++
-        h1.innerHTML = count+"%"
-        inside.style.width = count+"%"
-    }, 30);
-
+        grow++
+        h1.innerHTML = grow+"%"
+        inside.style.width = grow+"%"
+    },num);
+    
     setTimeout(() => {
-        btn.style.opacity = 0.7
         clearInterval(int)
         btn.innerHTML ="Downloaded"
-
-    },3000);
-
+        btn.style.opacity = 0.5
+    },num*100);
+    console.log(`Downlaod complete in ${num/10}`)
 })

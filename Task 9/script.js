11d5b2156fb5fdc890e218306acad06a9113006a
Jelaance0o <162440781+Jelaance0o@users.noreply.github.com>
2025-12-10 20:06:10 +0530

    querSelecterAll

--- Task 9/script.js
+++ Task 9/script.js
@@ -1,4 +1,16 @@
-var elem = document.querySelectorAll(".elem")
-console.log(elem);
+ var btn = document.querySelectorAll('button')
 
-all
+ btn.forEach(function(elem,indx)
+{
+    elem.addEventListener('click',function(){
+        if(elem.innerHTML == "Add Friend"){
+            console.log(indx)
+            elem.innerHTML = "Remove Friend"
+            elem.style.backgroundColor = 'red'
+        }
+        else{
+            elem.innerHTML ="Add Friend"
+            elem.style.backgroundColor = '#9eadff'
+        }
+    })
+})

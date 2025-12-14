fdee0d00ad64df02aa1ff1cdc1c1cfd719186f79
Jelaance0o <162440781+Jelaance0o@users.noreply.github.com>
2025-12-14 19:05:21 +0530

    Reels Complete

--- Task10/script.js
+++ Task10/script.js
@@ -1,5 +1,6 @@
 const reels = [
   {
+    ismuted: true,
     username: "Tech.Here",
     likeCount: 1200,
     isLiked: false,
@@ -12,6 +13,7 @@ const reels = [
       "https://i.pinimg.com/1200x/a1/93/7f/a1937fa703641b33e55ff4cd4d9ccfae.jpg",
   },
   {
+    ismuted: true,
     username: "Naimna",
     likeCount: 980,
     isLiked: true,
@@ -24,6 +26,7 @@ const reels = [
       "https://i.pinimg.com/736x/40/0c/29/400c29cbf963b48e9bf6cbd9189fcb90.jpg",
   },
   {
+    ismuted: true,
     username: "Sanlin",
     likeCount: 2400,
     isLiked: false,
@@ -36,6 +39,7 @@ const reels = [
       "https://i.pinimg.com/736x/28/bb/6c/28bb6c8e31908e20cbe049b0c1638eeb.jpg",
   },
   {
+    ismuted: true,
     username: "Nemon",
     likeCount: 750,
     isLiked: false,
@@ -48,6 +52,7 @@ const reels = [
       "https://i.pinimg.com/736x/4a/4c/87/4a4c87a3b7caadf7577418f78ad3d665.jpg",
   },
   {
+    ismuted: true,
     username: "Travel_Diaries",
     likeCount: 3100,
     isLiked: true,
@@ -60,6 +65,7 @@ const reels = [
       "https://i.pinimg.com/736x/d2/6e/31/d26e31e629d4d2d99a6d21fddf3e2035.jpg",
   },
   {
+    ismuted: true,
     username: "Food_heaven",
     likeCount: 660,
     isLiked: false,
@@ -72,6 +78,7 @@ const reels = [
       "https://i.pinimg.com/1200x/b5/27/8b/b5278b9d012e03db7c17587670c7f03d.jpg",
   },
   {
+    ismuted: true,
     username: "Music_wave",
     likeCount: 1870,
     isLiked: true,
@@ -84,6 +91,7 @@ const reels = [
       "https://i.pinimg.com/736x/28/bb/6c/28bb6c8e31908e20cbe049b0c1638eeb.jpg",
   },
   {
+    ismuted: true,
     username: "Artistic_mind",
     likeCount: 430,
     isLiked: false,
@@ -96,6 +104,7 @@ const reels = [
       "https://i.pinimg.com/736x/28/bb/6c/28bb6c8e31908e20cbe049b0c1638eeb.jpg",
   },
   {
+    ismuted: true,
     username: "Gaming_arena",
     likeCount: 2150,
     isLiked: true,
@@ -108,6 +117,7 @@ const reels = [
       "https://i.pinimg.com/736x/70/91/52/70915205331d76b6744517937cbbdf02.jpg",
   },
   {
+    ismuted: true,
     username: "Minimal_life",
     likeCount: 520,
     isLiked: false,
@@ -123,26 +133,28 @@ const reels = [
 
 
 var reel = document.querySelector("#reel");
-var allreels = document.querySelector(".allreels");                                            
-function addData(){
+var allreels = document.querySelector(".allreels");                                        
 
+
+function addData(){
   var sum = "";
-  
 reels.forEach(function (elem, idx) {
   sum =
     sum +
     `<div class="reel">
-                    <video autoplay loop muted src="${
-                      elem.video
-                    }"></video>                       
+    <video autoplay loop ${elem.ismuted ? "muted" : ""} src="${elem.video}">
+    </video>                       
+    <div class="mute" id=${idx}>
+    ${elem.ismuted?'<i class="ri-volume-mute-line"></i>':'<i class="ri-volume-up-line"></i>'}
+    </div>
                     <div class="bottom">
                         <div class="user">
 
                             <img src="${elem.userProfile}" alt="">
                             <h4>${elem.username}</h4>
                             <button id=${idx} class ="follow">${
-                              elem.isFollowed ? "Unfollow" : "Follow"
-                            }</button>
+      elem.isFollowed ? "Unfollow" : "Follow"
+    }</button>
                         </div>
                         <h3>${elem.caption}</h3>
                     </div>
@@ -186,6 +198,7 @@ allreels.addEventListener('click',function(dets){
     reels[dets.target.id].likeCount--;
     reels[dets.target.id].isLiked = false; 
   }}
+  addData()
 
   if(dets.target.className=='follow'){
     if(!reels[dets.target.id].isFollowed){
@@ -194,6 +207,18 @@ allreels.addEventListener('click',function(dets){
     else{
       reels[dets.target.id].isFollowed = false
     }
+    addData()
   }
-  addData()
+
+    if (dets.target.className == "mute") {
+      console
+      if (!reels[dets.target.id].ismuted) {
+        reels[dets.target.id].ismuted = true;
+      } else {
+        reels[dets.target.id].ismuted = false;
+
+      }
+      addData();
+    }
+
 })

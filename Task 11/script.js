f98a31d5737f8b6f197f2403d80ff9eb980a07d6
Jelaance0o <162440781+Jelaance0o@users.noreply.github.com>
2025-12-16 20:16:08 +0530

    Radial Gradient

---
+++ Task 11/script.js
@@ -0,0 +1,7 @@
+
+document.addEventListener("mousemove",(elem)=>{
+    console.log(elem)
+    document.body.style.setProperty("--x", elem.clientX + 'px')
+    document.body.style.setProperty("--y", elem.clientY + 'px')
+    
+})
